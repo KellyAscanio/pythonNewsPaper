c22775bf02b44cfd2fcb7cf0de11e40cefef1b06
KellyAscanio <101758695+KellyAscanio@users.noreply.github.com>
2022-12-17 11:29:41 -0500

    Delete venv-pynewspaper directory

--- venv-pynewspaper/app_newspaper/pages/views.py
+++
@@ -1,7 +0,0 @@
-from turtle import home
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-# Create your views here.
-class HomePageView(TemplateView):
-    template_name= "home.html"
